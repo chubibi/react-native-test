acb48d72daec8bcb13c69037dd3f5c97fbda87ac
ChubisJos <josue.cazares.10@gmail.com>
2024-01-09 18:03:49 -0600

    Add navigation doc and view home

--- App.js
+++ App.js
@@ -1,4 +1,10 @@
-
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 export default function App() {
-  //return ();
+  return (
+    <View style={styles.container}>
+      <Text/>
+    </View>
+  );
 }
---
+++ constants/Navigation.js
@@ -0,0 +1,8 @@
+import { NavigationContainer } from "@react-navigation/native";
+export default function Navigation (){
+    return(
+        <NavigationContainer>
+            
+        </NavigationContainer>
+    )
+}

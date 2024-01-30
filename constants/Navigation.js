3fcbdd7b619d34a8ccf37bc89a164ee4a77f5791
ChubisJos <josue.cazares.10@gmail.com>
2024-01-30 16:22:25 -0600

    Add Navigation and icons

--- constants/Navigation.js
+++ constants/Navigation.js
@@ -4,16 +4,29 @@ import { Home } from "../Views/tabScreens/Home";
 import { Explore } from "../Views/tabScreens/Explore";
 import { Saved } from "../Views/tabScreens/Saved";
 import { User } from "../Views/tabScreens/User";
+import { Ionicons, AntDesign } from '@expo/vector-icons';
+import { colors } from "./colors";
 
 const Tab = createBottomTabNavigator();
 
 function TabGroup() {
     return (
-        <Tab.Navigator screenOptions={{
+        <Tab.Navigator screenOptions={({route})=>({
             headerShown:false,
-            tabBarStyle: { position: 'absolute', backgroundColor: "transparent" },
-        }}>
-            <Tab.Screen name="Home" component={Home} />
+            tabBarShowLabel:false,
+            tabBarIcon: ({color, focused, size}) => {
+                let iconName;
+                if(route.name === "Home"){
+                    iconName = "home";
+                }
+            },
+            tabBarStyle: { position: 'absolute', backgroundColor: `${colors.white}` },
+        })}>
+            <Tab.Screen name="Home" component={Home} options={
+                {
+                   tabBarIcon: () => <AntDesign name="home" size={24} color={colors.secondaryColor}/>
+                }
+            } />
             <Tab.Screen name="Explore" component={Explore} />
             <Tab.Screen name="Saved" component={Saved} />
             <Tab.Screen name="User" component={User} />

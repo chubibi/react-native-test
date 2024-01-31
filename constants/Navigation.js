c250be4e1dad5906e6b42acd13d7abde65984e06
ChubisJos <josue.cazares.10@gmail.com>
2024-01-31 16:01:17 -0600

    Add focused icons

--- constants/Navigation.js
+++ constants/Navigation.js
@@ -4,31 +4,32 @@ import { Home } from "../Views/tabScreens/Home";
 import { Explore } from "../Views/tabScreens/Explore";
 import { Saved } from "../Views/tabScreens/Saved";
 import { User } from "../Views/tabScreens/User";
-import { Ionicons, AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 import { colors } from "./colors";
 
 const Tab = createBottomTabNavigator();
 
 function TabGroup() {
     return (
-        <Tab.Navigator screenOptions={({route})=>({
-            headerShown:false,
-            tabBarShowLabel:false,
-            tabBarIcon: ({color, focused, size}) => {
+        <Tab.Navigator screenOptions={({ route }) => ({
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarIcon: ({ color, focused, size }) => {
                 let iconName;
-                if(route.name === "Home"){
-                    iconName = "home";
+                if (route.name === "Home") {
+                    iconName = focused ? "home" : "home-outline";
+                } else if (route.name === "Explore") {
+                    iconName = focused ? "search" : "search-outline";
+                } else if (route.name === "Saved") {
+                    iconName = focused ? "airplane" : "airplane-outline";
                 }
+                return <Ionicons name={iconName} color={colors.secondaryColor} size={26} />
             },
             tabBarStyle: { position: 'absolute', backgroundColor: `${colors.white}` },
         })}>
-            <Tab.Screen name="Home" component={Home} options={
-                {
-                   tabBarIcon: () => <AntDesign name="home" size={24} color={colors.secondaryColor}/>
-                }
-            } />
-            <Tab.Screen name="Explore" component={Explore} />
-            <Tab.Screen name="Saved" component={Saved} />
+            <Tab.Screen name="Home" component={Home}/>
+            <Tab.Screen name="Explore" component={Explore}/>
+            <Tab.Screen name="Saved" component={Saved}/>
             <Tab.Screen name="User" component={User} />
         </Tab.Navigator>
     );
